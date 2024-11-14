4cab783b1e0a71503ed483f77a43a8391b4dd2d7
omininola <otaviomiklos@gmail.com>
2024-11-14 20:39:27 -0300

    "test'

--- lua/configs/plugins/fugitive.lua
+++ lua/configs/plugins/fugitive.lua
@@ -1,3 +1,2 @@
-vim.keymap.set("n", "<leader>g", vim.cmd.Git)
-vim.keymap.set("n", "<leader>ga", vim.cmd.Git("add ."))
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gp", vim.cmd.Git("push"))
