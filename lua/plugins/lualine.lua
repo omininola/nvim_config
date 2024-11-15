354382cd7170a14fcf1897a2de8f44e9ed384a5b
omininola <otaviomiklos@gmail.com>
2024-11-15 01:49:02 -0300

    Revert "lualine and zenmode"
    This reverts commit a52828f447423a5d7513f94c18e7cde988436f8e.

--- lua/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-    { -- Beautiful terminal
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-	    require('configs/plugins/lualine')
-	end,
-    },
-}
